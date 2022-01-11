d8feec02a7e1b34b6d44b21b56b28dd21e35ce5a
Zurina <bigler1205@gmail.com>
2022-01-11 10:56:03 +0100

    feat: added welcome controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use \App\Http\Controllers\WelcomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+# BOOK CONTROLLER
+Route::resource('/', 'App\Http\Controllers\WelcomeController');
