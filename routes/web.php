f3cc28fe93ee0eaecdf3d97ea7cd29343922fb31
Zurina <bigler1205@gmail.com>
2022-01-11 15:31:00 +0100

    feat: add tailwind template

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/welcome', function () {
+    return view('default_welcome');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
