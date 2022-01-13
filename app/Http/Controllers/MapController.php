2089adaac16999883c210bd9d6b83ad31ed3551e
Zurina <bigler1205@gmail.com>
2022-01-13 15:23:16 +0100

    feat: get all countries with addresses and send data to map

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Country;
 
 class MapController extends Controller
 {
@@ -12,7 +13,6 @@ class MapController extends Controller
             "chart" => array(
                 "caption" => "The international map",
                 "numbersuffix" => " student(s)",
-                "includevalueinlabels" => "1",
                 "labelsepchar" => ": ",
                 "entityFillHoverColor" => "#FFF9C4",
                 "theme" => "fusion"
@@ -28,17 +28,23 @@ class MapController extends Controller
         // ));
         // $arrMapConfig["colorRange"] = $colorDataObj;
 
-        $mapDataArray = array(
-            ["138", "1", "1"],
-            ["23", "5", "1"],
-            ["22", "2", "1"],
-            ["74", "3", "1"],
-            ["21", "10", "1"],
-            ["24", "13", "1"],
-            ["25", "15", "1"],
-            ["66", "11", "1"],
-            ["07", "7", "1"],
-        );
+        $countries = Country::has('addresses')->with('addresses')->get();
+
+        $mapDataArray = $countries->map(function ($country) {
+            return [$country['mapId'], count($country['addresses']), "1"];
+        });
+
+        // $mapDataArray = array(
+        //     ["138", "1", "1"],
+        //     ["23", "5", "1"],
+        //     ["22", "2", "1"],
+        //     ["74", "3", "1"],
+        //     ["21", "10", "1"],
+        //     ["24", "13", "1"],
+        //     ["25", "15", "1"],
+        //     ["66", "11", "1"],
+        //     ["07", "7", "1"],
+        // );
         
         $mapData = array();
 
