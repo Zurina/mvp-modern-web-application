43dbcd27939fa5e412ba10c1c7e7a8382b08bd63
Zurina <bigler1205@gmail.com>
2022-01-14 17:47:39 +0100

    refactor: routing

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,38 +1,41 @@
 <html>
-    <head>
-        <title>FusionCharts | My First Map</title>
-        
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.maps.js"></script>
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/maps/fusioncharts.worldwithcountries.js"></script>
-        <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
-        <link rel="stylesheet" href="{{ URL::asset('main.css') }}">
-    </head>
-    <body>
-      <x-map-layout mapConfig={!!$mapConfig!!}/>
 
-      <?php if ( $countryName != null ) { ?> 
+<head>
+  <title>FusionCharts | My First Map</title>
+
+  <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.js"></script>
+  <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/fusioncharts.maps.js"></script>
+  <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/maps/fusioncharts.worldwithcountries.js"></script>
+  <script type="text/javascript" src="https://cdn.fusioncharts.com/fusioncharts/latest/themes/fusioncharts.theme.fusion.js"></script>
+  <link rel="stylesheet" href="{{ URL::asset('main.css') }}">
+</head>
+
+<body>
+  <x-map-layout mapConfig={!!$mapConfig!!} />
+
+  <?php if ($countryName != null) { ?>
     <h1>{{$countryName}}</h1>
-      <table>
-          <thead>
-              <tr>
-                  <th>ID</th>
-                  <th>Name</th>
-                  <th>Email</th>
-              </tr>
-          </thead>
-          <tbody>
-              @foreach($students as $student)
-              <tr>
-                  <td>{{ $student['id'] }}</td>
-                  <td>{{ $student['name'] }}</td>
-                  <td>{{ $student['email'] }}</td>
-              </tr>
-              @endforeach
-          </tbody>
-      </table>
-      <?php } ?>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>More details</th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($students as $student)
+        <tr>
+          <td>{{ $student['id'] }}</td>
+          <td>{{ $student['name'] }}</td>
+          <td>{{ $student['email'] }}</td>
+          <td><a class="a-button" href="{{ URL::to('students/' . $student['id']) }}">More details</a></td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  <?php } ?>
+</body>
 
-    </body>
 </html>
-
