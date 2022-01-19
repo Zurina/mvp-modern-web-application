c69317f79a83a3ba041f9ea75d832569149c58ef
Zurina <bigler1205@gmail.com>
2022-01-19 21:50:06 +0100

    feat: validation for country selection

--- resources/views/components/dropdown-countries.blade.php
+++ resources/views/components/dropdown-countries.blade.php
@@ -2,7 +2,7 @@
     <label for="country_id" class="col-md-4 control-label">Select a country</label>
     <div class="col-md-6">
 
-        <select class="form-control" name="country_id" id="country_id">
+        <select class="form-control" name="country_id" id="country_id" required>
             <option value="">Select a country</option>
             @foreach ($countries as $country)
             <option value="{{ $country->id }}">{{ ucfirst($country->label) }}</option>
