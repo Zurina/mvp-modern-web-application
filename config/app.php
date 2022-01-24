006f82d2b0634ad5fe7dafaf22ea8bf400136738
Zurina <bigler1205@gmail.com>
2022-01-24 16:24:00 +0100

    feat: install laravel backup

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Spatie\Backup\BackupServiceProvider::class,
 
         /*
          * Package Service Providers...
