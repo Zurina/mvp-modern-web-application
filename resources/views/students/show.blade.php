8ccdca8ead58a675d0345802aecf10a07cfcc5c5
Zurina <bigler1205@gmail.com>
2022-01-26 15:09:05 +0100

    refactor: improve design of show student page

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -1,13 +1,20 @@
 <x-app-layout>
     <div>
-        <div style="text-align: center;">
+        <div style="display: flex; justify-content: space-around;">
             @if( Auth::id() == $student->id )
-            <h1>Welcome to your own page! You can configure your details here..</h1>
+            <span class="subsection-header">Welcome to your own page!</span>
+            <div>
+                <span class="subsection-header">Add address</span>
                 <x-create-address :countries="$countries" />
-                <a class="a-button" href="{{ route('students.edit', ['student' => $student->id]) }}">Edit this user</a>
+            </div>
             @endif
-            <h3>{{$student->name}}, {{$student->email}}</h3>
-            <a class="a-button" href="{{ route('personalMap', ['id' => $student->id]) }}">Show personal map</a>
+            <div style="display: flex; flex-direction: column;">
+                <span class="subsection-header">{{$student->name}}, {{$student->email}}</span>
+                <a class="link-button" href="{{ route('personalMap', ['id' => $student->id]) }}">Show personal map</a>
+                @if( Auth::id() == $student->id )
+                    <a class="link-button" href="{{ route('students.edit', ['student' => $student->id]) }}">Edit this user</a>
+                @endif
+            </div>
         </div>
         <table>
             <thead>
@@ -17,7 +24,7 @@
                     <th>Country</th>
                     <th>Current Address</th>
                     @if( Auth::id() == $student->id )
-                        <th>Actions</th>
+                    <th>Actions</th>
                     @endif
                 </tr>
             </thead>
@@ -29,14 +36,14 @@
                     <td>{{ $address->country->label }}</td>
                     <td>{{ $address['current_address'] == 1 ? 'True' : 'False' }}</td>
                     @if( Auth::id() == $student->id )
-                        <td>
-                            <a class="a-button" href="{{ route('addresses.edit', ['address' => $address]) }}">Edit this address</a>
-                            <form method="POST" action="{{ route('addresses.destroy', ['address' => $address]) }}">
-                                @csrf
-                                @method('delete')
-                                <x-input type="submit" value="Delete this address" onclick="return confirm('Are you sure you want to delete this address?')"/>
-                            </form>
-                        </td>
+                    <td>
+                        <a class="a-button" href="{{ route('addresses.edit', ['address' => $address]) }}">Edit this address</a>
+                        <form method="POST" action="{{ route('addresses.destroy', ['address' => $address]) }}">
+                            @csrf
+                            @method('delete')
+                            <x-input type="submit" value="Delete this address" onclick="return confirm('Are you sure you want to delete this address?')" />
+                        </form>
+                    </td>
                     @endif
                 </tr>
                 @endforeach
